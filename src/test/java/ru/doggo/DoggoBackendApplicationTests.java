83818c2ea694f22555dac1e0975719f3f65d2eb9
Sergio <afirthes@gmail.com>
2023-02-07 08:05:49 +0300

    Initial commit

---
+++ src/test/java/ru/doggo/DoggoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.doggo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoggoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
